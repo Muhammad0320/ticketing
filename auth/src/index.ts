f48d393c219a199c1d67f7af1365dd831d177270
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-18 06:06:30 +0100

    chore: update index ts file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
 
+console.log("Hi mom");
+
 const port = 3000;
+
+app.get("/api/users/getUser", (req: Request, res: Response) => {
+  res.send("Hi mom :)");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
