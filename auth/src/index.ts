891a094c3022cad9eef0bbe82af8b57b87d956d8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-19 09:24:36 +0100

    chore: update index js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,8 @@ app.use(express.json());
 
 console.log("Hi mom");
 
+console.log("Thnaks");
+
 app.get("/api/users/currentUser", (req: Request, res: Response) => {
   res.send("Hi mom :)");
 });
@@ -15,3 +17,5 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
+
+// muhammawwal@1123
