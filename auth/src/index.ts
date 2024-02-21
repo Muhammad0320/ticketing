dbd9a3ede1b53a96721855dae25523161d180395
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-21 04:51:53 +0100

    chore: define route definition

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,21 @@
 import express, { Request, Response } from "express";
+import { currentUserRouter } from "./routes/currentUser";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { signinRouter } from "./routes/signin";
 
 const app = express();
 
 app.use(express.json());
 
 console.log("Hi mom");
+
+const rootUserUrl = "/api/users";
+
+app.use(rootUserUrl, signupRouter);
+app.use(rootUserUrl, signoutRouter);
+app.use(rootUserUrl, signinRouter);
+app.use(rootUserUrl, currentUserRouter);
 
 const port = 3000;
 
