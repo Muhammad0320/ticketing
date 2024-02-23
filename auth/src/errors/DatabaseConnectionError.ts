c0401971d28ef5c74a64ad5788c6562628389bfb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-23 04:51:33 +0100

    chore: update database connection

--- auth/src/errors/DatabaseConnectionError.ts
+++ auth/src/errors/DatabaseConnectionError.ts
@@ -1,8 +1,17 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./CustomError";
+
+export class DatabaseConnectionError extends CustomError {
   reasons = "Error connecting to database";
+
+  statusCode = 500;
+
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeError() {
+    return [{ message: this.reasons }];
   }
 }
