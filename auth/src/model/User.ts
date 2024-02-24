d200e5556d189333823b0eb93a519e1f56950ffb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-24 19:58:13 +0100

    chore: add user schema

---
+++ auth/src/model/User.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+});
