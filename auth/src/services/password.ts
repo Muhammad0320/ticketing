fc4b6547df01169fcc951163b3637ff8a152266d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-25 08:12:01 +0100

    chore: update password hash static

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,7 +1,15 @@
-import { scrypt } from "node:crypto";
+import { scrypt, randomBytes } from "node:crypto";
 
 import { promisify } from "util";
 
 const scryptAsync = promisify(scrypt);
 
-export class Password {}
+export class Password {
+  static async hash(password: string): Promise<string> {
+    const salt = randomBytes(4).toString("hex");
+
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return `${buf}.${salt}`;
+  }
+}
