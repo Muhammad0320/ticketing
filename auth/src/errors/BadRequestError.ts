90ae2f6e92efe12aa667a40fb3c9c3d2895c25fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-25 05:13:00 +0100

    chore: update bad request error

--- auth/src/errors/BadRequestError.ts
+++ auth/src/errors/BadRequestError.ts
@@ -1,1 +1,15 @@
-export class BadRequestError {}
+import { CustomError } from "./CustomError";
+
+export class BadRequestError extends CustomError {
+  statusCode = 400;
+
+  constructor(public message: string) {
+    super();
+
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+
+  serializeError() {
+    return [{ message: this.message }];
+  }
+}
