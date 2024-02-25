93d3c6f6f49482f61b0e94d9ce4d0c1d2fa18073
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-25 08:16:31 +0100

    chore: update password

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -10,6 +10,17 @@ export class Password {
 
     const buf = (await scryptAsync(password, salt, 64)) as Buffer;
 
-    return `${buf}.${salt}`;
+    return `${buf.toString("hex")}.${salt}`;
+  }
+
+  static async compare(
+    storedPassword: string,
+    suppliedPassword: string
+  ): Promise<boolean> {
+    const [password, salt] = storedPassword.split(".");
+
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString("hex") === password;
   }
 }
