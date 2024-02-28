a505ac91387d153d8ff11aec1fe114d67d52ad96
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-28 04:54:09 +0100

    chore: update CurrentUser middlewate

--- auth/src/middlewares/currentUser.ts
+++ auth/src/middlewares/currentUser.ts
@@ -1,7 +1,26 @@
+import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
 
 export const currentUser = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+  } catch (error) {}
+
+  next();
+};
