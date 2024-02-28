3030cb6008959a342ae4b55f849e4bf940dcd7a0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-28 05:06:12 +0100

    chore: update Not authorized

--- auth/src/errors/NotAuthorized.ts
+++ auth/src/errors/NotAuthorized.ts
@@ -1,1 +1,15 @@
-class NotAuthorized {}
+import { CustomError } from "./CustomError";
+
+export class NotAuthorized extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super();
+
+    Object.setPrototypeOf(this, NotAuthorized.prototype);
+  }
+
+  serializeError() {
+    return [{ message: "Not Authorized, Please signin" }];
+  }
+}
