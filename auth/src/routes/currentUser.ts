d319c74b913f596446ed3c6594f71fece509d9a9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-28 04:23:04 +0100

    chore: update currentUser

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from "express";
-
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.get("/currentUser", (req: Request, res: Response) => {
-  res.send("hi mom");
+  if (!req.session?.jwt) {
+    return res.status(404).json({ status: "fail", currentUser: null });
+  }
+
+  try {
+    const currentUser = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    return res.status(200).json({ status: "success", currentUser });
+  } catch (error) {
+    return res.status(404).json({ status: "fail", currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
