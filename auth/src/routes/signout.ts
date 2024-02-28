e24a21e8ed03b702febc95eccce7694540199d69
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-28 04:24:33 +0100

    chore: update signout process

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,11 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.post("/signout", (req: Request, res: Response) => {
-  res.send("hi mom");
+  req.session = null;
+
+  return res
+    .status(200)
+    .json({ status: "success", message: "Logged out successfully" });
 });
 
 export { router as signoutRouter };
