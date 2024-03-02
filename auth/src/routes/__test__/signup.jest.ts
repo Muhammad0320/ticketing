8b366ef3525d7650214b4890aeb3293d31086c52
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-02 06:58:28 +0100

    chore: write test to asserts a 400 on invalid email

--- auth/src/routes/__test__/signup.jest.ts
+++ auth/src/routes/__test__/signup.jest.ts
@@ -7,3 +7,10 @@ it("returns a 201 successful signup", async () => {
     .send({ email: "hello@test.com", password: "passwords" })
     .expect(201);
 });
+
+it("returns 400 on invalid email", async () => {
+  return supertest(app)
+    .post("api/users/v1")
+    .send({ email: "shitititititit", password: "passwords" })
+    .expect(400);
+});
