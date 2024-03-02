0e63a1518a2d50904e0b16c84de0f8c6622d8f5e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-02 06:17:05 +0100

    chore: update signup jest

--- auth/src/routes/__test__/signup.jest.ts
+++ auth/src/routes/__test__/signup.jest.ts
@@ -1,1 +1,9 @@
-it("returns a 201 successful signup", async () => {});
+import supertest from "supertest";
+import { app } from "../../app";
+
+it("returns a 201 successful signup", async () => {
+  return supertest(app)
+    .post("/api/users/signup")
+    .send({ email: "hello@test.com", password: "passwords" })
+    .expect(201);
+});
