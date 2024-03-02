15bdf68cb2ff4c346854b9a3512c39e4ef329656
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-02 07:51:24 +0100

    chore: update signup test file

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,1 +1,9 @@
-it("returns 400 on invalid email", async () => {});
+import supertest from "supertest";
+import { app } from "../../app";
+
+it("returns 400 on invalid email", async () => {
+  return supertest(app)
+    .post("api/users/signin")
+    .send({ email: "test@example.com", password: "password" })
+    .expect(400);
+});
