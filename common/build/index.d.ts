10e7ac1e376b2b3821ebd2cdbc827cf3e9b79d1d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-05 08:58:54 +0100

    chore: update code

--- common/build/index.d.ts
+++
@@ -1,7 +0,0 @@
-interface Colors {
-    red: number;
-    blue: number;
-    green: number;
-}
-declare const colors: Colors;
-export default colors;
--- common/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hi mum");
-const colors = {
-    red: 23,
-    green: 23,
-    blue: 82,
-};
-exports.default = colors;
