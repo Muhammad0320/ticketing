dd7284690d2fda5f0abb42278e5811af5c931ade
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-05 09:07:37 +0100

    chore: update common dir

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,13 +1,13 @@
 console.log("Hi mum");
 
-import * as BadRequestError from "../errors/BadRequestError";
-import * as CustomError from "../errors/CustomError";
-import * as DatabaseConnectionError from "../errors/DatabaseConnectionError";
-import * as RequestValidationError from "../errors/RequestValidationError";
-import * as NotFound from "../errors/NotFound";
-import * as NotAuthorized from "../errors/NotAuthorized";
+export * from "../errors/BadRequestError";
+export * from "../errors/CustomError";
+export * from "../errors/DatabaseConnectionError";
+export * from "../errors/RequestValidationError";
+export * from "../errors/NotFound";
+export * from "../errors/NotAuthorized";
 
-import * as currentUser from "../middlewares/currentUser";
-import * as errorHandler from "../middlewares/errorHandler";
-import * as requireAuth from "../middlewares/requireAuth";
-import * as requestValidator from "../middlewares/requestValidator";
+export * from "../middlewares/currentUser";
+export * from "../middlewares/errorHandler";
+export * from "../middlewares/requireAuth";
+export * from "../middlewares/requestValidator";
