f5456a5a4221e461e03bb248da140c7261bb1ff0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-05 05:36:02 +0100

    chore: add index file for src

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+console.log("Hi mum");
+
+interface Colors {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const colors: Colors = {
+  red: 23,
+  green: 23,
+  blue: 82,
+};
+
+console.log(colors);
