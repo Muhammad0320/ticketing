45ab65d93b067a2cebc43d6dc9cb19a4592c4aec
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-05 05:47:07 +0100

    chore: add build dir for common dir

---
+++ common/build/index.d.ts
@@ -0,0 +1,6 @@
+interface Colors {
+    red: number;
+    blue: number;
+    green: number;
+}
+declare const colors: Colors;
---
+++ common/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hi mum");
+const colors = {
+    red: 23,
+    green: 23,
+    blue: 82,
+};
+console.log(colors);
