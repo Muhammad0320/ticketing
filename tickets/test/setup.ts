7f0b9886c65212f0cd3c552be0acd9621d684100
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-06 03:16:07 +0100

    chore: add test dir to tickets

---
+++ tickets/test/setup.ts
@@ -0,0 +1,45 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import supertest from "supertest";
+import { app } from "../app";
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "my-super-secure-and-ultra-long-secret-string-for-jwt";
+
+  mongo = await MongoMemoryServer.create();
+
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+
+  await mongoose.connection.close();
+});
+
+global.signin = async () => {
+  const response = await supertest(app)
+    .post("/api/users/signup")
+    .send({ email: "foo@example.com", password: "passwords" })
+    .expect(201);
+
+  return response.get("Set-Cookie");
+};
