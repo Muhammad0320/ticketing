1217cf5e597227ac2eb8563c3214ea18d0029bdf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-06 04:15:22 +0100

    chore: written skeleton for all tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,4 +2,10 @@ import supertest from "supertest";
 
 import { app } from "../../app";
 
-it("has a route handler of /api/tickets listening for post request", () => {});
+it("has a route handler of /api/tickets listening for post request", async () => {});
+
+it("can only be accessed when the user is logged in", async () => {});
+
+it("return as error if invalid title is provided", async () => {});
+
+it("return as error if invalid price is provided", async () => {});
