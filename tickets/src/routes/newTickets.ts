9ef9aa5125493799675302a3474fedf95a49fbe7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-07 07:08:56 +0100

    chore: update new tickets

--- tickets/src/routes/newTickets.ts
+++ tickets/src/routes/newTickets.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@m0ticketing/common";
+import { requireAuth, requestValidator } from "@m0ticketing/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/", requireAuth, (req: Request, res: Response) => {
-  res.status(200);
-});
+router.post(
+  "/",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Please provide a valid title"),
+
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Please provide a valid price"),
+  ],
+  requestValidator,
+  (req: Request, res: Response) => {
+    res.status(201).json({ status: "succcess", json: {} });
+  }
+);
