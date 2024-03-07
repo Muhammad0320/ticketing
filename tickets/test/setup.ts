74c5d7d731ea9c98b33672ba83f71e62a7c12a24
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-07 05:27:22 +0100

    chore: update setup ts for tickets

--- tickets/test/setup.ts
+++ tickets/test/setup.ts
@@ -1,7 +1,7 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import supertest from "supertest";
-import { app } from "../app";
+import { app } from "../src/app";
 
 declare global {
   var signin: () => Promise<string[]>;
