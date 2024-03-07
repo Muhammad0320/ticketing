b4ab620a768759fa91ee94caab08794c7c585536
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-07 07:14:40 +0100

    chore: update

--- tickets/model/tickets.ts
+++ tickets/model/tickets.ts
@@ -1,5 +1,17 @@
+import mongoose from "mongoose";
+
 interface TicketsAttrs {
   userId: string;
   title: string;
   price: number;
+}
+
+interface TicketsDoc extends mongoose.Document {
+  userId: string;
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketsDoc> {
+  buildTickets(attrs: TicketsAttrs): TicketsDoc;
 }
