e060d0cbf920e2ea8f31ae583eef57e9198c9542
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-08 07:05:11 +0100

    chore update show tickets:

--- tickets/src/routes/showTicket.ts
+++ tickets/src/routes/showTicket.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import { Ticket } from "../../model/tickets";
+import { NotFound } from "@m0ticketing/common";
 
 const router = express.Router();
 
 router.get("/:id", async (req: Request, res: Response) => {
   const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) new NotFound();
 
   res.status(200).json({ ticket });
 });
