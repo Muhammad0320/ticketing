9eaa2b0f394f69ebaf31625735312856c061892f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-08 07:15:44 +0100

    chore: update the show test file

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,3 +1,28 @@
-it("return 404 if no ticket with such id", async () => {});
+import supertest from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
 
-it("returns 200 if there is a ticket with such id", async () => {});
+it("return 404 if no ticket with such id", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await supertest(app).get(`/api/tickets/${id}`).send({}).expect(404);
+});
+
+it("returns 200 if there is a ticket with such id", async () => {
+  const title = "New Ticket";
+  const price = 999;
+
+  const response = await supertest(app)
+    .post(`/api/tickets`)
+    .set("Cookie", global.signin())
+    .send({ title, price })
+    .expect(201);
+
+  const ticketResponse = await supertest(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send({})
+    .expect(200);
+
+  expect(ticketResponse[0].title).toEqual(title);
+  expect(ticketResponse[0].price).toEqual(price);
+});
