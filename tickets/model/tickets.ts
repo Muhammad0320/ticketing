66c61455c715b0dee2efc51d9e19d49e476a3485
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-08 05:22:17 +0100

    chore: formatted the string on new tickets

--- tickets/model/tickets.ts
+++ tickets/model/tickets.ts
@@ -17,22 +17,33 @@ interface TicketModel extends mongoose.Model<TicketsDoc> {
   buildTickets(attrs: TicketsAttrs): Promise<UserDoc>;
 }
 
-const ticketSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please provide a valid title"],
-  },
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please provide a valid title"],
+    },
 
-  price: {
-    type: String,
-    required: [true, "Please provide a valid price"],
-  },
+    price: {
+      type: String,
+      required: [true, "Please provide a valid price"],
+    },
 
-  userId: {
-    type: String,
-    required: true,
+    userId: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+
+        delete ret._id;
+      },
+    },
+  }
+);
 
 ticketSchema.statics.buildTicket = async (attrs: TicketsAttrs) => {
   const newDoc = await Ticket.create(attrs);
