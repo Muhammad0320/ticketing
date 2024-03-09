f1f45167877a0f6b2817e4987e39a5f615f27637
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-09 07:05:16 +0100

    chore: add test to check for invalid id

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,1 +1,13 @@
-it("return a 404 if the id is not valid", async () => {});
+import mongoose from "mongoose";
+import supertest from "supertest";
+import { app } from "../../app";
+
+it("return a 404 if the id is not valid", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await supertest(app)
+    .put(`/api/tickets/${id}`)
+    .set("Cookie", global.signin())
+    .send({})
+    .expect(404);
+});
