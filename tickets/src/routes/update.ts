b7c0f67e798f7b21e0d0fe5a323187a385e2f057
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-09 07:34:01 +0100

    chore: added error case for updates ts

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,8 +1,19 @@
-import { requireAuth } from "@m0ticketing/common";
+import { NotAuthorized, NotFound, requireAuth } from "@m0ticketing/common";
 import express, { Request, Response } from "express";
+import { Ticket } from "../../model/tickets";
 
 const router = express.Router();
 
-router.put("/", requireAuth, async (req: Request, res: Response) => {});
+router.put("/:id", requireAuth, async (req: Request, res: Response) => {
+  const tickets = await Ticket.findById(req.params.id); //
+
+  if (!tickets) {
+    throw new NotFound();
+  }
+
+  if (tickets.userId !== req.currentUser.id) {
+    throw new NotAuthorized();
+  }
+});
 
 export { router as updateTicketRouter };
