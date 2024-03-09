3775aa3f79aeccf2da7bfecdec8c1270601a3a7a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-09 07:07:39 +0100

    chore: addd a test to chek for anauthorized updates

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -8,6 +8,15 @@ it("return a 404 if the id is not valid", async () => {
   await supertest(app)
     .put(`/api/tickets/${id}`)
     .set("Cookie", global.signin())
-    .send({})
+    .send({ title: "nrjvnrjvbr", price: 23 })
     .expect(404);
+});
+
+it("return a 401, if the user is nit authroized", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await supertest(app)
+    .put(`/api/tickets/${id}`)
+    .send({ title: "nrjvnrjvbr", price: 23 })
+    .expect(401);
 });
