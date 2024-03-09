06f0a8f0329180825793c969eb91ac311a65a111
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-09 07:45:47 +0100

    chore: validated the request

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,19 +1,30 @@
 import { NotAuthorized, NotFound, requireAuth } from "@m0ticketing/common";
 import express, { Request, Response } from "express";
 import { Ticket } from "../../model/tickets";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.put("/:id", requireAuth, async (req: Request, res: Response) => {
-  const tickets = await Ticket.findById(req.params.id); //
+router.put(
+  "/:id",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Please provide a valid title"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Please provide a price thats greater than 0"),
+  ],
+  async (req: Request, res: Response) => {
+    const tickets = await Ticket.findById(req.params.id); //
 
-  if (!tickets) {
-    throw new NotFound();
-  }
+    if (!tickets) {
+      throw new NotFound();
+    }
 
-  if (tickets.userId !== req.currentUser.id) {
-    throw new NotAuthorized();
+    if (tickets.userId !== req.currentUser.id) {
+      throw new NotAuthorized();
+    }
   }
-});
+);
 
 export { router as updateTicketRouter };
