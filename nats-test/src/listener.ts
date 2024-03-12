c903f49b142f41e82afc35d643c74506f5b78659
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-13 00:51:18 +0100

    chore: add listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,9 +1,9 @@
 import nats from "node-nats-streaming";
 
-const stan = nats.connect("ticketing", "abc", {
+const stan = nats.connect("ticketing", "123", {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
-  console.log("Publisher connected to NATS");
+  console.log("Listener connected to NATS");
 });
