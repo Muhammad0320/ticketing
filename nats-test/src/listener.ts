f5fb3351645b437059415afa35561c28dc0af4a7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-14 05:26:10 +0100

    chore: update listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
 
 const stan = nats.connect("ticketing", "123", {
   url: "http://localhost:4222",
@@ -9,7 +9,10 @@ stan.on("connect", () => {
 
   const subscription = stan.subscribe("ticket:created");
 
-  subscription.on("message", (msg) => {
-    console.log("Message received");
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    typeof data === "string" &&
+      console.log(`Event #${msg.getSequence()} received with data : ${data} `);
   });
 });
