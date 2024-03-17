489311352073ce9e381f76b8684591f3f071792f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-17 10:57:24 +0100

    chore: update base publisher

--- nats-test/events/BasePublisher.ts
+++ nats-test/events/BasePublisher.ts
@@ -1,1 +1,23 @@
-export abstract class BasePublisher {}
+import { Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+
+interface Events {
+  subjects: Subjects;
+  data: any;
+}
+
+export abstract class BasePublisher<T extends Events> {
+  abstract subjects: Subjects;
+
+  private client: Stan;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  publish(subject: T["subjects"], data: T["data"]) {
+    this.client.publish(subject, data, () => {
+      console.log(`Sussessfully published ${subject} Events`);
+    });
+  }
+}
