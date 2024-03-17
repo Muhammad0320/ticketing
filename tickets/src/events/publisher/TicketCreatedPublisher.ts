35525d1476b61a64b617b9feb0113e5557db33bf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-17 17:17:53 +0100

    chore: update ticket created publihser

--- tickets/src/events/publisher/TicketCreatedPublisher.ts
+++ tickets/src/events/publisher/TicketCreatedPublisher.ts
@@ -1,1 +1,9 @@
-export class TicketCreatedPublisher {}
+import {
+  BasePublisher,
+  Subjects,
+  TicketCreatedEvent,
+} from "@m0ticketing/common";
+
+export class TicketCreatedPublisher extends BasePublisher<TicketCreatedEvent> {
+  readonly subjects = Subjects.TicketCreated;
+}
