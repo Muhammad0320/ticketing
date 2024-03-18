d24c474991bab31c5e447d71d21c973c60c4d299
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-18 19:59:02 +0100

    chore: update nats wrapper

--- tickets/natsWrapper.ts
+++ tickets/natsWrapper.ts
@@ -4,17 +4,25 @@ import nats from "node-nats-streaming";
 class NatsWrapper {
   private _client?: Stan;
 
+  client() {
+    if (!this._client) {
+      throw new Error(" Nats client not connected ");
+    }
+
+    return this._client;
+  }
+
   connect(clusterId: string, clientId: string, url: string): Promise<void> {
     this._client = nats.connect(clusterId, clientId, { url });
 
     return new Promise((resolve, reject) => {
-      this._client!.on("connect", () => {
+      this.client().on("connect", () => {
         console.log("Connected to NATS");
 
         resolve();
       });
 
-      this._client!.on("error", (error: any) => {
+      this.client().on("error", (error: any) => {
         console.log("Unable to connect to NATS ");
 
         reject(error);
