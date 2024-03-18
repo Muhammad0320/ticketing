91d707edbefade037ce30d11ea7e6c9787ac7434
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-18 20:16:11 +0100

    chore: update index.ts file

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,3 +1,4 @@
+import { natsWrapper } from "../natsWrapper";
 import { app } from "./app";
 import mongoose from "mongoose";
 
@@ -12,6 +13,17 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect("ticketing", "shitt", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection close!");
+
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("Connected to mongoDB");
