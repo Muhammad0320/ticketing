4cf4f5dc5fa22fa476fe1d96a9322bd54d8a232a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-19 05:02:49 +0100

    chore: add ticket updated publisher

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
 import express, { Request, Response } from "express";
 import { Ticket } from "../../model/tickets";
 import { body } from "express-validator";
+import { TicketUpdatedPublisher } from "../events/publisher/TicketUpdatedListener";
+import { natsWrapper } from "../../natsWrapper";
 
 const router = express.Router();
 
@@ -38,6 +40,13 @@ router.put(
     });
 
     await tickets.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: tickets.id,
+      title: tickets.title,
+      price: tickets.price,
+      userId: tickets.userId,
+    });
 
     res.status(200).json({ tickets });
   }
