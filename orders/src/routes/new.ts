4abd74f311533f9fa7c77e4c201d2b462065c69e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-20 17:29:35 +0100

    chore: validate request and addd auth

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import { requestValidator, requireAuth } from "@m0ticketing/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.post("/", (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Valid ticketId must be provided"),
+  ],
+  requestValidator,
+  (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
