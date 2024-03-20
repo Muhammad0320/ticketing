70d0f38515e224238ebe6616d006fb5b20ad3e34
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-20 18:12:07 +0100

    chore: update order mode file

--- orders/src/model/order.ts
+++ orders/src/model/order.ts
@@ -13,25 +13,38 @@ interface OrderModel extends mongoose.Model<OrderDoc> {
   build(attrs: OrderAttrs): OrderDoc;
 }
 
-const orderSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: [true, "An order must belong to a user"],
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: [true, "An order must belong to a user"],
+    },
+
+    status: {
+      type: String,
+      required: [true, "An order must have a status"],
+    },
+
+    expiredAt: mongoose.Schema.Types.Date,
+
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+
+      ref: "Ticket",
+
+      required: [true, "An order must belong to a ticket"],
+    },
   },
 
-  status: {
-    type: String,
-    required: [true, "An order must have a status"],
-  },
-
-  expiredAt: mongoose.Schema.Types.Date,
-
-  ticket: {
-    type: mongoose.Schema.Types.ObjectId,
-
-    required: [true, "An order must belong to a ticket"],
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 const Order = mongoose.model<OrderDoc, OrderModel>("order", orderSchema);
 
