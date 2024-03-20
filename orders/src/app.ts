a480fedb8ac8f4d733fa915c985196b324f7e0ea
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-20 03:41:35 +0100

    chore: copied the app and index file of tickets to orders

---
+++ orders/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { NotFound, errorHandler } from "@m0ticketing/common";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    httpOnly: true,
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+console.log("Hi mom");
+
+app.all("*", (req, res) => {
+  throw new NotFound();
+});
+
+app.use(errorHandler);
+
+export { app };
