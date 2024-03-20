30922e74482efc97977e79b9c83e2cdd403e59bb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-20 17:07:06 +0100

    chore: update new order handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.post("/", (req: Request, res: Response) => {
   res.send({});
 });
 
-export { router as indexRouter };
+export { router as newOrderRouter };
