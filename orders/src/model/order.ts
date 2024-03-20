0fcbd966bc6b801c279032b2d5abe4eace178b18
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-20 18:09:36 +0100

    chore: create order service schema and model

--- orders/src/model/order.ts
+++ orders/src/model/order.ts
@@ -12,3 +12,27 @@ type OrderDoc = mongoose.Document & OrderAttrs;
 interface OrderModel extends mongoose.Model<OrderDoc> {
   build(attrs: OrderAttrs): OrderDoc;
 }
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: [true, "An order must belong to a user"],
+  },
+
+  status: {
+    type: String,
+    required: [true, "An order must have a status"],
+  },
+
+  expiredAt: mongoose.Schema.Types.Date,
+
+  ticket: {
+    type: mongoose.Schema.Types.ObjectId,
+
+    required: [true, "An order must belong to a ticket"],
+  },
+});
+
+const Order = mongoose.model<OrderDoc, OrderModel>("order", orderSchema);
+
+export { Order };
