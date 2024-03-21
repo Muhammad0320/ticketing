288b19f0cc8c9c378eda0135c7aec8da42f54c91
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-21 18:12:55 +0100

    chore: add ticket schema

--- orders/src/model/tickets.ts
+++ orders/src/model/tickets.ts
@@ -6,8 +6,20 @@ interface TicketAttrs {
   title: string;
 }
 
-type TicketDoc = mongoose.Document & TicketAttrs;
+export type TicketDoc = mongoose.Document & TicketAttrs;
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
+
+const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Tickets must have a title"],
+  },
+
+  price: {
+    type: String,
+    required: [true, "Tickets must have a price"],
+  },
+});
