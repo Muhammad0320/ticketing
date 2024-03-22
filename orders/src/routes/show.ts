aa547477c29c34242dc267592b82cbf063435b80
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 12:36:43 +0100

    chore: add login fir getting one og YOUR orders

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from "express";
+import { Order } from "../model/order";
+import { NotAuthorized, NotFound, requireAuth } from "@m0ticketing/common";
 
 const router = express.Router();
 
-router.get("/:orderId", (req: Request, res: Response) => {
-  res.send({});
+router.get("/:orderId", requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId);
+
+  if (!order) {
+    throw new NotFound();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorized();
+  }
+
+  res.status(200).json({ status: "success", data: order });
 });
 
 export { router as showOrderRouter };
