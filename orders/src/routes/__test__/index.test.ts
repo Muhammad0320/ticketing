d15a3b7f34f6de6fbec5b0ea2fe1ee9aa7b798e6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 11:50:00 +0100

    chore:  created 3 tickets

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,1 +1,11 @@
-it("fetches order for a particular user", async () => {});
+import { Ticket } from "../../model/tickets";
+
+const ticketBuilder = async () => {
+  return await Ticket.create({ price: 20, title: "Football game" });
+};
+
+it("fetches order for a particular user", async () => {
+  const ticketOne = await ticketBuilder();
+  const ticketTwo = await ticketBuilder();
+  const ticketThree = await ticketBuilder();
+});
