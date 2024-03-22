48bb1c1c10d2fe7bef5856766ce7c9aa5b32e179
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 04:55:17 +0100

    chore: awrite test to return error if the ticket doenot exist

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,4 +1,16 @@
-it("returns an error if ticket does not exist", async () => {});
+import mongoose from "mongoose";
+import supertest from "supertest";
+import { app } from "../../app";
+
+it("returns an error if ticket does not exist", async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await supertest(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
 
 it("returns an error if the ticket is alrealy reserved", async () => {});
 
