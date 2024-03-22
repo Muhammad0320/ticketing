6a0f64b9d9cfe03baa61e2093a00b5c44720b8bf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 04:47:51 +0100

    chore: add new ts test suite

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,5 @@
+it("returns an error if ticket does not exist", async () => {});
+
+it("returns an error if the ticket is alrealy reserved", async () => {});
+
+it("reserves a ticket", async () => {});
