2e0dce4878e1ee230a4f0c118c0924f0d1d0950b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 13:23:43 +0100

    chor update delete ts file:

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,1 +1,21 @@
-it("cancells an order", async () => {});
+import supertest from "supertest";
+import { Ticket } from "../../model/tickets";
+import { app } from "../../app";
+
+it("cancells an order", async () => {
+  const ticket = await Ticket.build({ price: 99, title: "Hajj" });
+
+  const user = global.signin();
+
+  const { body: order } = await supertest(app)
+    .post("/api/order")
+    .set("Cookie", user)
+    .send({ ticket: ticket.id })
+    .expect(201);
+
+  await supertest(app)
+    .delete(`/api/order${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+});
