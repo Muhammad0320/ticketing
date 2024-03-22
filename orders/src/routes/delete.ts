225b2399ddf80737247980523fd672fb9c468562
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 13:15:51 +0100

    chore: addorder cancelling logic

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,29 @@
 import express, { Request, Response } from "express";
+import { Order, OrderStatus } from "../model/order";
+import { NotAuthorized, NotFound } from "@m0ticketing/common";
 
 const router = express.Router();
 
-router.delete("/:orderId", (req: Request, res: Response) => {
-  res.send({});
+router.delete("/:orderId", async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    throw new NotFound();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorized();
+  }
+
+  // logic
+
+  order.status = OrderStatus.Cancelled;
+
+  await order.save();
+
+  res.status(204).json("success");
 });
 
 export { router as deleteOrderRouter };
