543905943640f0d41879dc5001cf180aac134b25
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 12:52:37 +0100

    chore: add test to assert a user cannot view other users order

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,3 +1,25 @@
-it("returns an error if a user tries to get anpther user's order", async () => {});
+import supertest from "supertest";
+import { Ticket } from "../../model/tickets";
+import { app } from "../../app";
+
+it("returns an error if a user tries to get anpther user's order", async () => {
+  const ticket = await supertest(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ price: 40, title: "shit shit " })
+    .expect(201);
+
+  const order = await supertest(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticket: ticket.body.id })
+    .expect(201);
+
+  await supertest(app)
+    .get(`/api/orders/${order.body.id}`)
+    .set("Cookie", global.signin())
+    .send({})
+    .expect(401);
+});
 
 it("shows an order from the current user orders", async () => {});
