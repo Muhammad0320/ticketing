1e874bf6b44815d710d073e1ba2c024cf001833d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-22 12:05:42 +0100

    chore: write all necessary logic for testing

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,4 +1,6 @@
+import supertest from "supertest";
 import { Ticket } from "../../model/tickets";
+import { app } from "../../app";
 
 const ticketBuilder = async () => {
   return await Ticket.create({ price: 20, title: "Football game" });
@@ -8,4 +10,31 @@ it("fetches order for a particular user", async () => {
   const ticketOne = await ticketBuilder();
   const ticketTwo = await ticketBuilder();
   const ticketThree = await ticketBuilder();
+
+  const cookieOne = global.signin();
+
+  const cookieTwo = global.signin();
+
+  await supertest(app)
+    .post("/api/orders")
+    .set("Cookie", cookieOne)
+    .send({ ticket: ticketOne })
+    .expect(201);
+
+  const { body: orderOne } = await supertest(app)
+    .post("/api/orders")
+    .set("Cookie", cookieTwo)
+    .send({ ticket: ticketTwo })
+    .expect(201);
+
+  const { body: orderOnorderTwo } = await supertest(app)
+    .post("/api/orders")
+    .set("Cookie", cookieTwo)
+    .send({ ticket: ticketThree })
+    .expect(201);
+
+  const response = await supertest(app)
+    .get("/api/orders")
+    .set("Cookie", cookieTwo)
+    .send({});
 });
