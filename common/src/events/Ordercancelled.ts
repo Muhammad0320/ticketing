58c3c5ae5a07856661768c169bead3b3984d9392
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-23 04:01:39 +0100

    chore: add order cancelled interface

--- common/src/events/Ordercancelled.ts
+++ common/src/events/Ordercancelled.ts
@@ -1,1 +1,14 @@
-export interface OrderCancelled {}
+import { Subjects } from "./subjects";
+
+export interface OrderCancelled {
+  subject: Subjects.OrderCancelled;
+
+  data: {
+    id: string;
+    userId: string;
+
+    ticket: {
+      id: string;
+    };
+  };
+}
