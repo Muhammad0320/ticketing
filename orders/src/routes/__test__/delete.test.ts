ec37191f5a08e5ce775d72cc88b54f9156dfbc13
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-23 05:15:47 +0100

    chore: add test to make sure that out order cancel is oublished

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import supertest from "supertest";
 import { Ticket } from "../../model/tickets";
 import { app } from "../../app";
 import { OrderStatus } from "../../model/order";
+import { natsWrapper } from "../../../natsWrapper";
 
 it("cancells an order", async () => {
   const ticket = await Ticket.build({ price: 99, title: "Hajj" });
@@ -29,4 +30,22 @@ it("cancells an order", async () => {
   expect(orderResponse.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits an event when order is cancelled");
+it("emits an event when order is cancelled", async () => {
+  const ticket = await Ticket.build({ price: 999, title: "Hajj" });
+
+  const user = global.signin();
+
+  const { body: order } = await supertest(app)
+    .post("/api/order")
+    .set("Cookie", user)
+    .send({ ticket: ticket.id })
+    .expect(201);
+
+  await supertest(app)
+    .delete(`/api/order${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
