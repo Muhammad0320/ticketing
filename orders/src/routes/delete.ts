b7d1b991c45ebe7141c05bc7f7d6f90e9dda1cbb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-23 05:04:27 +0100

    chore: update delete ts

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import { Order, OrderStatus } from "../model/order";
 import { NotAuthorized, NotFound } from "@m0ticketing/common";
+import { OrderCancelledPublisher } from "../events/publisher/OrderCancelledPublisher";
+import { natsWrapper } from "../../natsWrapper";
 
 const router = express.Router();
 
 router.delete("/:orderId", async (req: Request, res: Response) => {
   const { orderId } = req.params;
 
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate("ticket");
 
   if (!order) {
     throw new NotFound();
@@ -22,6 +24,12 @@ router.delete("/:orderId", async (req: Request, res: Response) => {
   order.status = OrderStatus.Cancelled;
 
   await order.save();
+
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    status: order.status,
+    ticket: { id: order.ticket.id },
+  });
 
   res.status(204).json("success");
 });
