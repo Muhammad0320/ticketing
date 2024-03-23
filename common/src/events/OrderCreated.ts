8dc52e3054e9c36d77dd63ad2db644275cc733f4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-23 04:06:42 +0100

    chore: add order created interface

--- common/src/events/OrderCreated.ts
+++ common/src/events/OrderCreated.ts
@@ -1,1 +1,18 @@
-export interface OrderCreated {}
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/OrderStatus";
+
+export interface OrderCreated {
+  subject: Subjects.OrderCreted;
+
+  data: {
+    id: number;
+    userId: string;
+    staus: OrderStatus.Created;
+    expiresAt: string;
+
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
