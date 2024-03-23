963381f72942e2041ed17a3378c92502a46f36b9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-23 04:57:54 +0100

    chore: update common dir

--- common/src/events/OrderCreated.ts
+++ common/src/events/OrderCreated.ts
@@ -1,6 +1,8 @@
 import { Subjects } from "./subjects";
 import { OrderStatus } from "./types/OrderStatus";
 
+// I love writing shit
+
 export interface OrderCreatedEvent {
   subjects: Subjects.OrderCreated;
 
