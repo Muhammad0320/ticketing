966e1c5c29e943e17f87848c6827f2a05f599dd1
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-24 04:51:18 +0100

    chore: update ticket listener

--- orders/src/events/listener/TicketCreatedListener.ts
+++ orders/src/events/listener/TicketCreatedListener.ts
@@ -5,13 +5,18 @@ import {
   TicketUpdatedEvent,
 } from "@m0ticketing/common";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../model/tickets";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subjects = Subjects.TicketCreated;
 
   queueGroupName = "TicketCreated";
 
-  onMesage(data: TicketUpdatedEvent["data"], msg: Message) {
+  async onMesage(data: TicketUpdatedEvent["data"], msg: Message) {
     const { id, title, price } = data;
+
+    await Ticket.create({ id, title, price });
+
+    msg.ack();
   }
 }
