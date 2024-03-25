12975dc404c5c7d029d285c7bb3495bc0aa9c489
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-25 05:22:48 +0100

    chore: add set up function usable fir both test

--- orders/src/events/listener/__test__/TicketCreatedListener.test.ts
+++ orders/src/events/listener/__test__/TicketCreatedListener.test.ts
@@ -1,3 +1,28 @@
+import { TicketCreatedEvent } from "@m0ticketing/common";
+import { natsWrapper } from "../../../../natsWrapper";
+import { TicketCreatedListener } from "../TicketCreatedListener";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new TicketCreatedListener(natsWrapper.client);
+
+  const data: TicketCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 33,
+    title: "concert",
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
 it("Creates and saves a new ticket");
 
 it("calls the ack function");
