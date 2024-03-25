eef96ef9a654697d66fd2929167bb056cd4a1597
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-25 11:50:35 +0100

    chore: addset up function for tests

--- orders/src/events/listener/__test__/TicketUpdatedListener.test.ts
+++ orders/src/events/listener/__test__/TicketUpdatedListener.test.ts
@@ -1,4 +1,34 @@
-const setup = async () => {};
+import mongoose from "mongoose";
+import { natsWrapper } from "../../../../natsWrapper";
+import { Ticket } from "../../../model/tickets";
+import { TicketUpdatedListener } from "../TicketUpdatedListener";
+import { TicketUpdatedEvent } from "@m0ticketing/common";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  const ticket = await Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 99,
+  });
+
+  const data: TicketUpdatedEvent["data"] = {
+    price: 999,
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: "new concert",
+    userId: "shitttt",
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
 
 it("finds update and saves a ticket", async () => {});
 
