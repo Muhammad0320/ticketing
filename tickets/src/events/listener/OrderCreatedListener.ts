cf4bb29785b6fe061f795376070b0fef0a749968
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-26 08:17:48 +0100

    chore: update order created listeners ts

--- tickets/src/events/listener/OrderCreatedListener.ts
+++ tickets/src/events/listener/OrderCreatedListener.ts
@@ -1,14 +1,24 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@m0ticketing/common";
 import { queueGroupName } from "./queueGroupName";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../model/tickets";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subjects = Subjects.OrderCreated;
 
   queueGroupName = queueGroupName;
 
-  async onMesage(
-    data: OrderCreatedEvent["data"],
-    msg: Message
-  ): Promise<void> {}
+  async onMesage(data: OrderCreatedEvent["data"], msg: Message): Promise<void> {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: data.id });
+
+    await ticket.save();
+
+    msg.ack();
+  }
 }
