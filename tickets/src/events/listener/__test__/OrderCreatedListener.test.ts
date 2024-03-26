02c8809aa7886f23c3b9f7ebfac07b9e08eb7c3c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-26 08:35:46 +0100

    test: add the neccessagry stuff to the set up function

--- tickets/src/events/listener/__test__/OrderCreatedListener.test.ts
+++ tickets/src/events/listener/__test__/OrderCreatedListener.test.ts
@@ -1,1 +1,35 @@
-const setup = () => {};
+import { OrderCreatedEvent, OrderStatus } from "@m0ticketing/common";
+import { Ticket } from "../../../../model/tickets";
+import { natsWrapper } from "../../../../natsWrapper";
+import { OrderCreatedListener } from "../OrderCreatedListener";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const ticket = await Ticket.buildTickets({
+    userId: "shitt",
+    title: "concert",
+    price: 99,
+  });
+
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: "string",
+    status: OrderStatus.Created,
+    expiresAt: "shottt",
+
+    version: 0,
+    ticket: {
+      id: new mongoose.Types.ObjectId().toHexString(),
+
+      price: 99,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+};
