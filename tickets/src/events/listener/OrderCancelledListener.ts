50cd5d07aa5d9e60a974029a63e7fcb7b18784b6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-27 04:50:55 +0100

    chore: add order cancelled event

--- tickets/src/events/listener/OrderCancelledListener.ts
+++ tickets/src/events/listener/OrderCancelledListener.ts
@@ -1,10 +1,34 @@
 import { Listener, OrderCancelledEvent, Subjects } from "@m0ticketing/common";
 import { queueGroupName } from "./queueGroupName";
+import { Ticket } from "../../../model/tickets";
+import { Message } from "node-nats-streaming";
+import { TicketUpdatedPublisher } from "../publisher/TicketUpdatedListener";
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
   readonly subjects = Subjects.OrderCancelled;
 
   queueGroupName = queueGroupName;
 
-  onMesage() {}
+  async onMesage(data: OrderCancelledEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: undefined });
+
+    await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      version: ticket.version,
+      price: ticket.price,
+      orderId: ticket.orderId,
+      userId: ticket.userId,
+    });
+
+    msg.ack();
+  }
 }
