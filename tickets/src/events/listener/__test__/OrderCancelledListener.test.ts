573affc95e655b986348efbe17b2ed3ef94b8637
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-27 05:08:33 +0100

    chore: add setup function

--- tickets/src/events/listener/__test__/OrderCancelledListener.test.ts
+++ tickets/src/events/listener/__test__/OrderCancelledListener.test.ts
@@ -1,1 +1,26 @@
-const setup = async () => {};
+import { OrderCancelledEvent, OrderStatus } from "@m0ticketing/common";
+import { natsWrapper } from "../../../../natsWrapper";
+import { OrderCancelledListener } from "../OrderCancelledListener";
+import mongoose from "mongoose";
+import { Ticket } from "../../../../model/tickets";
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+
+  const ticket = await Ticket.buildTickets({
+    userId: "shittt",
+    price: 22,
+    title: "concert",
+  });
+
+  const data: OrderCancelledEvent["data"] = {
+    id: orderId,
+    version: 0,
+    status: OrderStatus.Cancelled,
+    ticket: { id: ticket.id },
+  };
+
+  return { listener, orderId, ticket, data };
+};
