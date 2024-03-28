cd137277fbbf4c35da322a7d9af1b0608d060c3d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-28 06:25:31 +0100

    chore: add expiration service

---
+++ expiration/natsWrapper.ts
@@ -0,0 +1,34 @@
+import { Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      throw new Error(" Cannot access NATS client befire initialization ");
+    }
+
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string): Promise<void> {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log("Connected to NATS");
+
+        resolve();
+      });
+
+      this.client.on("error", (error: any) => {
+        console.log("Unable to connect to NATS ");
+
+        reject(error);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
