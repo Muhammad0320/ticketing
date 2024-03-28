b58ae22f809a4fa7f261837710d1250ef2da9312
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-28 10:15:04 +0100

    chore: update query queue

--- expiration/queues/expirationQueue.ts
+++ expiration/queues/expirationQueue.ts
@@ -1,3 +1,11 @@
+import Queue from "bull";
+
 interface Payload {
   orderId: string;
 }
+
+const expiration = new Queue("order:expiration", {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
