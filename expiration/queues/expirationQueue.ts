92dcc8950ca24a244cad40dda4e6a1f5b8a55dd4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-28 11:35:33 +0100

    chore: update expiration queue ts

--- expiration/queues/expirationQueue.ts
+++ expiration/queues/expirationQueue.ts
@@ -4,8 +4,15 @@ interface Payload {
   orderId: string;
 }
 
-const expiration = new Queue("order:expiration", {
+const expiration = new Queue<Payload>("order:expiration", {
   redis: {
     host: process.env.REDIS_HOST,
   },
+});
+
+expiration.process(async (job) => {
+  console.log(
+    "I want to publish order:expired event for order id: ",
+    job.data.orderId
+  );
 });
