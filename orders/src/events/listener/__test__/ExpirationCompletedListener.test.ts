8f7a2507f71a5181dfc6999eeaa5363c24ffa56e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-29 04:35:14 +0100

    chore: add setup functions for test

--- orders/src/events/listener/__test__/ExpirationCompletedListener.test.ts
+++ orders/src/events/listener/__test__/ExpirationCompletedListener.test.ts
@@ -1,1 +1,39 @@
-const setup = async () => {};
+import mongoose from "mongoose";
+import { natsWrapper } from "../../../../natsWrapper";
+import { Ticket } from "../../../model/tickets";
+import { ExpirationCompletedListener } from "../ExpirationCompleteListener";
+import { Order, OrderStatus } from "../../../model/order";
+import { ExpirationCompleteEvent } from "@m0ticketing/common";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new ExpirationCompletedListener(natsWrapper.client);
+
+  const ticket = await Ticket.build({
+    price: 20,
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "shit concert",
+
+    version: 0,
+  });
+
+  const order = await Order.build({
+    userId: "shitt user",
+
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+
+    ticket,
+    version: 0,
+  });
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+};
