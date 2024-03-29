3f83a435bf37d31303a9e41f894aa19f14bbf7fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-29 17:09:47 +0100

    chore: complete order schema

--- payment/src/models/order.ts
+++ payment/src/models/order.ts
@@ -10,21 +10,49 @@ type OrderAttrs = {
 
 type OrderDocs = mongoose.Document & OrderAttrs;
 
-interface OrderModel extends mongoose.Model<OrderDocs> {}
+interface OrderModel extends mongoose.Model<OrderDocs> {
+  buildOrder(attrs: OrderAttrs): Promise<OrderDocs>;
+}
 
-const orderSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: [true, "An order must belong to a user"],
-  },
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: [true, "An order must belong to a user"],
+    },
 
-  price: {
-    type: Number,
-    required: [true, "An order have a price"],
-  },
+    price: {
+      type: Number,
+      required: [true, "An order have a price"],
+    },
 
-  status: {
-    type: String,
-    required: [true, "An order must have a status"],
+    status: {
+      type: String,
+      required: [true, "An order must have a status"],
+    },
   },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.buildOrder = async (
+  attrs: OrderAttrs & { id: string }
+): Promise<OrderDocs> => {
+  return await Order.create({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDocs, OrderModel>("Order", orderSchema);
+
+export { Order };
