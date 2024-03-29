e8a6dabd42a0dd052befdfd792f3a999d65b4b5d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-29 12:04:52 +0100

    chore: update tikcet src

---
+++ payment/src/app.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { NotFound, currentUser, errorHandler } from "@m0ticketing/common";
+import { indexRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
+import { newTicketRouter } from "./routes/newTickets";
+import { showTicketRouter } from "./routes/showTicket";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    httpOnly: true,
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+console.log("Hi mom");
+
+app.use(currentUser);
+
+const rootUrl = "/api/tickets";
+
+app.use(rootUrl, indexRouter);
+app.use(rootUrl, newTicketRouter);
+app.use(rootUrl, showTicketRouter);
+app.use(rootUrl, updateTicketRouter);
+
+app.all("*", (req, res) => {
+  throw new NotFound();
+});
+
+app.use(errorHandler);
+
+export { app };
