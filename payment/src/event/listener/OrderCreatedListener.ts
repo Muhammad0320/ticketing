a18f47bca5411e651715292af98ad982f0abd04a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-29 17:40:18 +0100

    chore: update ordercreated listener

--- payment/src/event/listener/OrderCreatedListener.ts
+++ payment/src/event/listener/OrderCreatedListener.ts
@@ -1,3 +1,27 @@
-import { Listener, OrderCreatedEvent } from "@m0ticketing/common";
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@m0ticketing/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import mongoose from "mongoose";
+import { queueGroupName } from "./queueGroupName";
 
-export class OrderCreatedListener extends Listener<OrderCreatedEvent> {}
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subjects = Subjects.OrderCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMesage(data: OrderCreatedEvent["data"], msg: Message) {
+    await Order.buildOrder({
+      userId: new mongoose.Types.ObjectId().toHexString(),
+      version: 0,
+      price: data.ticket.price,
+      status: OrderStatus.Created,
+    });
+
+    msg.ack();
+  }
+}
