38a6e9ae68cee72b22d6bf1913d671a9e6e1cbe0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-30 12:15:31 +0100

    chore: update new test ts file

--- payment/src/routes/__tests__/new.test.ts
+++ payment/src/routes/__tests__/new.test.ts
@@ -2,6 +2,7 @@ import supertest from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
 import { OrderStatus } from "@m0ticketing/common";
+import { Order } from "../../models/order";
 
 it("return a 404 when user tries to pay for an order that doesn't exist", async () => {
   await supertest(app)
@@ -32,4 +33,24 @@ it("returns a 401 when purchasing an order that does not belong to the user", as
     .set("Cookie", global.signin())
     .send({ orderId: body.id, token: "neifeifeni" })
     .expect(401);
+});
+
+it("returns a 400 when purchasing an item with cancelled status", async () => {
+  const order = await Order.buildOrder({
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 99,
+    status: OrderStatus.Created,
+    id: new mongoose.Types.ObjectId().toHexString(),
+  });
+
+  order.set({ status: OrderStatus.Cancelled });
+
+  await order.save();
+
+  await supertest(app)
+    .post("api/payments")
+    .set("Cookie", global.signin())
+    .send({ order: order.id, token: "neifeifeni" })
+    .expect(400);
 });
