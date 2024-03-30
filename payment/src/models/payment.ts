8b7e01daa3afadc58d38e093139b239f595c4755
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-30 18:11:07 +0100

    chore: add payment scehma

--- payment/src/models/payment.ts
+++ payment/src/models/payment.ts
@@ -10,3 +10,25 @@ type PaymentDoc = mongoose.Document & PaymentAttrs;
 interface PaymentModel extends mongoose.Model<PaymentDoc> {
   buildPayment(attrs: PaymentAttrs): Promise<PaymentDoc>;
 }
+
+const paymentSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: [true, "A payment must have an orderId"],
+    },
+
+    stripeId: {
+      type: String,
+      required: [true, "A payment must have a stripeId"],
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
