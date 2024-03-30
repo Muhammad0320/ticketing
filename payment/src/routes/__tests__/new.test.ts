e941e66be8f84958d3adb3bc09b55e611119e656
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-30 11:59:49 +0100

    test:  add new test that return a 404 when user tries to pay for an order that does not exist

--- payment/src/routes/__tests__/new.test.ts
+++ payment/src/routes/__tests__/new.test.ts
@@ -1,1 +1,14 @@
-it("return a 404 when user tries to pay for an order that doesn't exist", async () => {});
+import supertest from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+
+it("return a 404 when user tries to pay for an order that doesn't exist", async () => {
+  await supertest(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+      token: "jrnjrgbjgbgbjgrbj",
+    })
+    .expect(404);
+});
