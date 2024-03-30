eef4b1cd1cbdd67cbbf1ed094953599af8e44976
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-03-30 03:58:21 +0100

    chore: add securite to the payment route

--- payment/src/routes/new.ts
+++ payment/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { requestValidator, requireAuth } from "@m0ticketing/common";
-import express from "express";
+import {
+  BadRequestError,
+  NotAuthorized,
+  NotFound,
+  OrderStatus,
+  requestValidator,
+  requireAuth,
+} from "@m0ticketing/common";
+import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -12,5 +20,23 @@ router.post(
     body("orderId").not().isEmpty().withMessage("User id is required"),
   ],
   requestValidator,
-  () => {}
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFound();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Can't pay for a cancelled order");
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorized();
+    }
+
+    res.json({ status: 200, data: { status: "success" } });
+  }
 );
